7656c183a50ae513925d2ebc58d024c081bd79b4
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-24 10:56:07 -0300

    Add project files.

---
+++ Par_Impar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Par_Impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, soma;            
+            Console.WriteLine("Informe o primeiro numero!");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o segundo numero!");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            soma = n1 + n2;
+            if ((n1 + n2) % 2 == 0)
+                Console.WriteLine(" A soma dos numeros e {0}, entao o numero e PAR.", soma);
+            else
+                Console.WriteLine("A soma dos numeros e {0}, entao o numero e IMPAR.", soma);
+
+        }
+    }
+}
